2b1f478cc93c879e5e4314b733f37b7ba2cbd5ef
zuoyao <1914202330@qq.com>
2018-01-07 17:17:52 +0800

    另一个添加测试

--- ssmATBookstore/src/test/AddTest2.java
+++ ssmATBookstore/src/test/AddTest2.java
@@ -4,5 +4,6 @@ public class AddTest2 {
     public static void main(String[] args) {
         System.out.println("xingzheng");
         System.out.println("修改1");
+        System.out.println("这是另一个添加测试");
     }
 }
