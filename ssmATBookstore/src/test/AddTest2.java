6a44d6edfef3de378017df903d4a754aa2c39cea
zuoyao <1914202330@qq.com>
2018-01-07 15:55:26 +0800

    新增修改AddTest2

--- ssmATBookstore/src/test/AddTest2.java
+++ ssmATBookstore/src/test/AddTest2.java
@@ -3,6 +3,6 @@ package test;
 public class AddTest2 {
     public static void main(String[] args) {
         System.out.println("xingzheng");
-        System.out.println("修改");
+        System.out.println("修改1");
     }
 }
